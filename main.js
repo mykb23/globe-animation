7d0f4023f4286673cc66c2c8304a6a45a7ad19a6
Michael Akinsuroju <makinsuroju@gmail.com>
2022-01-24 08:57:30 +0100

    fixed build issues

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Globe from 'globe.gl';
 import data from './data.js';
+import imgUrl from '/img/8k_earth_daymap.jpeg';
 // import { fabric } from 'fabric';
 
 const world = Globe({
@@ -10,7 +11,7 @@ const world = Globe({
 	.showAtmosphere(true)
 	.atmosphereColor('white')
 	.atmosphereAltitude(0.15)
-	.globeImageUrl('./img/8k_earth_daymap.jpeg')
+	.globeImageUrl(imgUrl)
 	.arcsData(data['arcs'])
 	.arcColor('color')
 	.arcDashLength(() => Math.random())
